6a5e8ba0d2630c444a49c5c9f6e23452c2ced86d
Ilshat <illliat@mail.ru>
2022-12-11 10:17:52 +0300

    CreateArray

---
+++ CreateArray/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая задает массив из 8 элементов
+//и выводит их на экран
+
+//Метод для заполнения массива случайными числами от 1 до 99
+void FillArray(int[] array)
+{
+    int min = 1;
+    int max = 99;
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+}
+//Создание пустого массива из 8 элементов
+int length = 8;
+int[] array = new int[length];
+//Тело программы
+FillArray(array);
+//Вывод результата
+Console.WriteLine($"[{string.Join(", ", array)}]");
